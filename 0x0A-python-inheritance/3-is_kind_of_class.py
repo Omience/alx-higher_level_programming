75f823f797c75fefde57c6e338bda37ba1db9947
Omience <patfun10@gmail.com>
2022-08-03 22:56:18 +0000

    0

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -4,7 +4,6 @@
 
 def is_kind_of_class(obj, a_class):
     """Check if an object is an instance or inherited instance of a class.
-    
     Args:
         obj (any): The object to check.
         a_class (type): The class to match the type of obj to.
@@ -14,4 +13,4 @@ def is_kind_of_class(obj, a_class):
     """
     if isinstance(obj, a_class):
         return True
-    return False 
+    return False
